b131a40ab4c581e7817d7e8b2edfc5a084dc84aa
cesar <cesarv01@gmail.com>
2018-08-15 13:29:08 +0100

    - updating documentation  time: 13:29:08 date: 15-08-2018

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m " - updating blog  time: $(date +"%T") date: $(date +"%d-%m-%Y")"
+git commit -m " - updating documentation  time: $(date +"%T") date: $(date +"%d-%m-%Y")"
 git push origin master
